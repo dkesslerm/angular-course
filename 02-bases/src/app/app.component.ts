64dad28b189b86980f369a4abae4b5c9fecccc74
David Kessler <dkesslerm03@correo.ugr.es>
2023-07-27 14:58:00 +0200

    Bloque 4 terminado

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -7,18 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Hola Mundo';
-  public counter: number = 10;
-
-  increaseBy(value: number): void{
-    this.counter += value;
-  }
-
-  decreaseBy(value: number): void{
-    this.counter -= value;
-  }
-
-  resetCounter(): void{
-    this.counter = 10;
-  }
 
 }
